f2be3ae1c04f651a0e2cd9436adbf95398d8fc77
Michael Mosmann <michael@mosmann.de>
2015-08-16 11:09:23 +0200

    lic header

--- src/test/resources/mocks/my-prog.bat
+++ src/test/resources/mocks/my-prog.bat
@@ -1,2 +1,26 @@
+@REM
+@REM Copyright (C) 2011
+@REM   Michael Mosmann <michael@mosmann.de>
+@REM   Martin Jöhren <m.joehren@googlemail.com>
+@REM
+@REM with contributions from
+@REM 	konstantin-ba@github,
+	Archimedes Trajano (trajano@github),
+	Kevin D. Keck (kdkeck@github),
+	Ben McCann (benmccann@github)
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM         http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 Dummy Exe
 
