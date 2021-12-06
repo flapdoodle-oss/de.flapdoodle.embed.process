cca3a89dfa4432d3673a5084c59461d4d511c98a
Michael Mosmann <michael@mosmann.de>
2021-12-06 21:50:31 +0100

    merge new stuff into old packages

--- src/main/java/de/flapdoodle/embed/process/transitions/PackageOfDistribution.java
+++ src/main/java/de/flapdoodle/embed/process/transitions/PackageOfDistribution.java
@@ -21,7 +21,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.flapdoodle.embed.processg.parts;
+package de.flapdoodle.embed.process.transitions;
 
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.config.store.Package;
